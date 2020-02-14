e27873ee83bfadf85a21423a3027baa010b34b62
Angel Lopez Manriquez <sigma271@outlook.com>
2020-02-14 09:54:15 -0600

    Add files via upload

--- 7/main.cpp
+++ 7/main.cpp
@@ -2,11 +2,28 @@
 
 using namespace std;
 
-int main() {
+// ? 
+void a() {
     int n;
     string line;
     cin >> n;
     getline(cin, line);
     cout << line ;
+}
+
+// A pesar de haber usado at, sigue arrojando una excepcion
+void b() {
+    // string cadena1("ESCOM");
+    // cout << cadena1[6] << endl;
+    string cadena2("ESCOM");
+    cout << cadena2.at(6) << endl;
+}
+
+void c() {
+    
+}
+
+int main() {
+    a();
     return 0;
 }
