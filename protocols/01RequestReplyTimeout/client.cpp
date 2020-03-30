c8b36e1e4bc1839d578d879962c00e0226f346b6
Angel Lopez Manriquez <sigma271@outlook.com>
2020-03-30 02:44:44 -0600

    antes de modificar la longitud

--- protocols/01RequestReplyTimeout/client.cpp
+++ protocols/01RequestReplyTimeout/client.cpp
@@ -7,20 +7,24 @@ int main(int argc, char const *argv[]) {
     int arr[2] = {2, 2};
     string ip("127.0.0.1");
     uint16_t port = 5400;
+    int n = 1000;
 
-    if (argc != 4) {
-        printf("[%s] [ip] [int] [int]\n", argv[0]);
-        printf("Se ejecutara en el localhost, sumando %d con %d\n", arr[0], arr[1]);
+    if (argc != 5) {
+        printf("%s [ip] [int] [int] [N_REQUEST]\n", argv[0]);
+        printf("Se ejecutara en el localhost, sumando %d con %d %d veces\n", arr[0], arr[1], n);
     } else {
         ip = argv[1];
         arr[0] = atoi(argv[2]);
         arr[1] = atoi(argv[3]);
+        n = atoi(argv[4]);
     }
 
     Request request(ip, port);
-    int *result = (int *) request.doOperation( OperationId::SUM, (char *) arr, sizeof(arr), 2, 0);
-    if (result == NULL) 
-        throw "NULL returned";
-    printf("%d + %d = %d\n", arr[0], arr[1], *result);
+    for (int i = 0; i < n; ++i) {
+        int *result = (int *) request.doOperation( OperationId::SUM, (char *) arr, sizeof(arr), 2, 0);
+        if (result == NULL) 
+            throw "NULL returned";
+        printf("%d + %d = %d\n", arr[0], arr[1], *result);
+    }
     return 0;
 }
--- protocols/01RequestReplyTimeout/server.cpp
+++ protocols/01RequestReplyTimeout/server.cpp
@@ -8,7 +8,7 @@ char *sum(char *args_) {
     int *args = (int *) args_;
     int *res = new int(1);
     *res = args[0] + args[1];
-    printf("%d + %d = %d", args[0], args[1], *res);
+    printf("%d + %d = %d\n", args[0], args[1], *res);
     return (char *) res; 
 }
 
@@ -16,13 +16,17 @@ int main(int argc, char const *argv[]) {
     const int port = 5400;
     Reply reply(port);
     cout << "El servido ha empezado, esperando solicitud..." << endl;
-    Message *msg = reply.getRequest();
-    if (msg->operationId == OperationId::SUM) {
-        char *result = sum(msg->arguments);
-        reply.sendReply(result, sizeof(int), OperationId::SUM);
-    } else {
-        cerr << "Operacion no reconocida" << endl;
-        // send NULL
+    int request_count = 0;
+    while (1) {
+        Message *msg = reply.getRequest();
+        printf("Request %d ", ++request_count);
+        if (msg->operationId == OperationId::SUM) {
+            char *result = sum(msg->arguments);
+            reply.sendReply(result, sizeof(int), OperationId::SUM);
+        } else {
+            cerr << "Operacion no reconocida" << endl;
+            // send NULL
+        }
     }
     puts("");
     return 0;
