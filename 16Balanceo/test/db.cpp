9fb8d154948b1b6b0f4a2c4ca3e9dbfaf17d5465
ang3lino <sigma271@outlook.com>
2020-05-15 22:27:27 -0500

    cambios

--- 16Balanceo/test/db.cpp
+++ 16Balanceo/test/db.cpp
@@ -1,13 +1,7 @@
 
 #include "../lib/sqlite3.h"
 #include <bits/stdc++.h>
-#include <varargs.h>
 
-void db_query(...) 
-{
-    char *query;
-    asprintf(query, __VA_ARGS__);
-}
 
 sqlite3 *db_open(char *fname) 
 {
@@ -17,9 +11,31 @@ sqlite3 *db_open(char *fname)
     return db;
 }
 
+template<typename ... Args>
+std::string string_format( const std::string& format, Args ... args )
+{
+    size_t size = snprintf( nullptr, 0, format.c_str(), args ... ) + 1; // Extra space for '\0'
+    if( size <= 0 ){ throw std::runtime_error( "Error during formatting." ); }
+    std::unique_ptr<char[]> buf( new char[ size ] ); 
+    snprintf( buf.get(), size, format.c_str(), args ... );
+    return std::string( buf.get(), buf.get() + size - 1 ); // We don't want the '\0' inside
+}
+
+bool db_query(sqlite3 *db, std::string &query) {
+    char *err = NULL;
+    sqlite3_exec(db, query.c_str(), NULL, NULL, &err);
+    if (err != NULL) {
+        printf("%s\n", err);
+        return false;
+    }
+    free(err);
+    return true;
+}
+
 int main(int argc, char const *argv[])
 {
     sqlite3 *db = db_open(NULL);
-    
+    std::string query = "CREATE TABLE t (id INT AUTOINCREMENT PRIMARY KEY);";
+    db_query(db, query);
     return 0;
 }
