52aad2b45a8e189a8b756e728026966ea13c56ac
ang3lino <sigma271@outlook.com>
2020-05-16 18:44:45 -0500

    sqlite API done

--- 16Balanceo/test/db.cpp
+++ 16Balanceo/test/db.cpp
@@ -1,5 +1,6 @@
 
 #include "../lib/sqlite3.h"
+#include "../Trie.cpp"
 #include <bits/stdc++.h>
 
 
@@ -21,21 +22,115 @@ std::string string_format( const std::string& format, Args ... args )
     return std::string( buf.get(), buf.get() + size - 1 ); // We don't want the '\0' inside
 }
 
-bool db_query(sqlite3 *db, std::string &query) {
+// std::unique_ptr<char *>
+char *db_query(sqlite3 *db, std::string query) 
+{
     char *err = NULL;
     sqlite3_exec(db, query.c_str(), NULL, NULL, &err);
     if (err != NULL) {
         printf("%s\n", err);
-        return false;
     }
-    free(err);
-    return true;
+    return err;    
+}
+
+std::string str_from_file(std::string fname) 
+{
+    std::ifstream in(fname);
+    std::stringstream buff;
+    buff << in.rdbuf();
+    return buff.str();
+}
+
+struct ResultSet {
+    int cols;
+    Trie<int> name2int;
+
+    std::vector<std::vector<std::string > > tuples;
+
+    ResultSet(): cols(0) {
+    }
+
+    ResultSet(int argc, char **column): cols(argc) {
+        set_column_names(argc, column);
+    }
+
+    unsigned row_count() { return tuples.size(); }
+
+    void print() {
+        for (unsigned i = 0; i < row_count(); ++i) {
+            std::cout << i << ": ";
+            for (auto &value: tuples[i]) {
+                std::cout << value + ", ";
+            }
+            std::cout << endl;
+        }
+        std::cout << endl;
+        std::cout << endl;
+    }
+
+    int col_index(std::string key) {
+        return name2int.get(key);
+    }
+
+    void set_column_names(int argc, char **column) {
+        if (cols == 0) {
+            cols = argc;
+            for (int i = 0; i < argc; ++i) {
+                std::string key = column[i];
+                name2int.put(key, i);
+            }
+        }
+    }
+
+    void put(char **values) {
+        std::vector<std::string > tuple(cols);
+        for (int i = 0; i < cols; ++i) {
+            tuple[i] = values[i];
+        }
+        tuples.push_back(tuple);
+    }
+};
+
+int the_callback(void *array_in, int argc, char **argv, char **column) 
+{
+    ResultSet *rset = (ResultSet *) array_in;
+    rset->set_column_names(argc, column); 
+    rset->put(argv);
+    return 0;
+}
+
+ResultSet db_query_values(sqlite3 *db, std::string query) 
+{
+    char *err = NULL;
+    ResultSet rset;
+    sqlite3_exec(db, query.c_str(), the_callback, &rset, &err);
+    if (err != NULL) {
+        printf("%s\n", err);
+        free(err);
+    }
+    return rset;
 }
 
 int main(int argc, char const *argv[])
 {
     sqlite3 *db = db_open(NULL);
-    std::string query = "CREATE TABLE t (id INT AUTOINCREMENT PRIMARY KEY);";
-    db_query(db, query);
+    std::string db_def = str_from_file("db.sql"); // define db arch
+    char *err = db_query(db, db_def);
+
+    // insertions
+    for (int i = 0; i < 10; ++i) {
+        auto query = string_format(
+            "INSERT INTO registro VALUES ('%d', 'LOMA980824HDFPNN03', 'PRI', 1.584);", i);
+        db_query(db, query);
+    }
+
+    // read
+    ResultSet rset = db_query_values(db, "SELECT * FROM registro;");
+    int idx = rset.col_index("timestamp");
+    for (auto &tuple: rset.tuples) {
+        cout << tuple[idx] << endl;
+    }
+    if (err != NULL)
+        free(err);
     return 0;
 }
